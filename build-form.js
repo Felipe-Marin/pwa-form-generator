614ff7ba66f5d3b2241cc29be282cc82d21090a2
Felipe-Marin <felipmarin5@gmail.com>
2019-06-24 02:08:45 -0300

    update google

--- build-form.js
+++ build-form.js
@@ -1,16 +1,19 @@
-#!/usr/bin/env node
-
 const fs = require('fs');
 const ejs = require('ejs');
+const readline = require('readline');
+const {google} = require('googleapis');
 
 //carrega arquivo de configurações
-const configFile = 'src/config.json';
+const configFile = 'config.json';
 const config = JSON.parse(fs.readFileSync(configFile));
 
 config.forms = numerateForms(config.forms);
 generateFormsComponents(config.forms);
 generateFormsModule(config);
 generateFormsList(config);
+if(config.google_sheets){
+  generateGoogleSheet(config);
+}
 
 /**
  * Numera forms
@@ -85,3 +88,212 @@ function generateFormsList(config){
 //gera arquivos com configurações gerais do projeto
 
 //gera planilha do google sheets e web service no app scripts
+
+function generateGoogleSheet(config){
+  let sheetList =  [];
+  for(let i = 0; i < config.forms.length; i++){
+    let keys = [];
+    for(let j = 0; j < config.forms[i].questions.length; j++){
+      keys.push(config.forms[i].questions[j].id);
+    }
+    let sheet = {name: config.forms[i].id, keys: keys};
+    sheetList.push(sheet);
+  }
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Apps Script API.
+    authorize(JSON.parse(content), callAppsScript, config.filename, sheetList);
+  });
+}
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/script.projects', 
+  'https://www.googleapis.com/auth/script.deployments',
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/spreadsheets'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback, filename, sheetList) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, generateSourceCode(filename, sheetList), generateManifestCode());
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Creates a new script project, upload a file, and log the script's URL.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function callAppsScript(auth, sourceCode, manifestCode) {
+  const script = google.script({version: 'v1', auth});
+  script.projects.create({
+    resource: {
+      title: 'Sheet Service',
+    },
+  }, (err, res) => {
+    if (err) return console.log(`The API create method returned an error: ${err}`);
+    script.projects.updateContent({
+      scriptId: res.data.scriptId,
+      auth,
+      resource: {
+        files: [{
+          name: 'main',
+          type: 'SERVER_JS',
+          source: sourceCode,
+        }, {
+          name: 'appsscript',
+          type: 'JSON',
+          source: manifestCode,
+        }],
+      },
+    }, {}, (err, res) => {
+      if (err) return console.log(`The API updateContent method returned an error: ${err}`);
+      console.log(`https://script.google.com/d/${res.data.scriptId}/edit`);
+      script.projects.versions.create({
+        scriptId: res.data.scriptId,
+        auth
+      }, (err, res) => {
+        if (err) return console.log(err);
+        script.projects.deployments.create({
+          scriptId: res.data.scriptId,
+          auth,
+          resource: {  
+            versionNumber: res.data.versionNumber,
+            manifestFileName: 'appsscript',
+            description: 'Description'
+          }
+        }, (err, res) => {
+          if (err) return console.log(err);
+          console.log(`https://script.google.com/macros/s/${res.data.deploymentId}/exec`);
+        });
+      });
+    });
+  });
+}
+
+
+function generateSourceCode(filename, sheetList){
+  let sourceCode = `
+    var filename = "${filename}";
+    var sheetList = [${sheetList.map(sheet => {return `{name: '${sheet.name}', keys: [${sheet.keys.map(key  => {return `'${key}'`})}]}`})}];
+    var spreadsheet = null;
+    var files = DriveApp.searchFiles('mimeType = "' + MimeType.GOOGLE_SHEETS + '"');
+    while (files.hasNext()) {
+      var file = files.next();
+      if(file.getName() == filename){
+        spreadsheet = SpreadsheetApp.open(file);
+      }
+    }
+    if(spreadsheet == null){
+      spreadsheet = SpreadsheetApp.create(filename);
+    }
+    
+    for(var i = 0; i < sheetList.length; i++){
+      if(!spreadsheet.getSheetByName(sheetList[i].name)){
+        spreadsheet.insertSheet(sheetList[i].name)
+      }
+    }
+  
+    
+  
+    function doGet(e) {
+      var requestStatus = null;
+      var message = null;
+      var output = null;
+      var rowContents = [];
+      if(!('sheetName' in e.parameters)){
+        requestStatus = 'error';
+        message = 'missing sheet name';
+        output = JSON.stringify({status: requestStatus, message: message});
+        return ContentService.createTextOutput(output).setMimeType(ContentService.MimeType.JSON);
+      }
+      var sheetName = e.parameters['sheetName'][0];
+      if(sheetList.map(function(sheets) { return sheets.name; }).indexOf(sheetName) < 0){
+        requestStatus = 'error';
+        message = 'invalid sheet name';
+        output = JSON.stringify({sheetName: sheetName, sheetList: sheetList, status: requestStatus, message: message});
+        return ContentService.createTextOutput(output).setMimeType(ContentService.MimeType.JSON);
+      }
+      var sheet = spreadsheet.getSheetByName(sheetName);
+      var keyList = sheetList.filter(function(sheets){return sheets.name == sheetName})[0].keys;
+      for(var i = 0; i < keyList.length; i++){
+        var key = keyList[i];
+        if(!(key in e.parameters)){
+          requestStatus = 'error';
+          message = 'missing values';
+          output = JSON.stringify({status: requestStatus, message: message});
+          return ContentService.createTextOutput(output).setMimeType(ContentService.MimeType.JSON);
+        }else{
+          rowContents.push(e.parameters[key][0]);
+        }
+      }
+      sheet.appendRow(rowContents);
+      var output = JSON.stringify({status: 'success', message: 'success'});
+      return ContentService.createTextOutput(output).setMimeType(ContentService.MimeType.JSON);
+    }
+  `;
+  return sourceCode;
+}
+function generateManifestCode(){
+  const manifestCode = `
+    {
+      "timeZone": "America/Sao_Paulo",
+      "dependencies": {
+      },
+      "webapp": {
+        "access": "ANYONE_ANONYMOUS",
+        "executeAs": "USER_DEPLOYING"
+      },
+      "exceptionLogging": "STACKDRIVER"
+    }
+  `
+  return manifestCode;
+}
